e758062d44e69cee33d223ccefd3c9ffe20c59c0
Clarkie <clarkie@concrete.cc>
2015-05-14 22:39:47 +0100

    fix lint issues

--- index.js
+++ index.js
@@ -12,22 +12,22 @@ var jenkins = require('jenkins')(jenkinsUrl);
 
 var screen = blessed.screen();
 
-var grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
+var grid = new contrib.grid({rows: 12, cols: 12, screen: screen});
 
 var defaultStyle = { fg: 'white', bg: 'black', border: { fg: '#f0f0f0' } };
 
-var addJobToGrid = function (job, grid, x, y, w, h) {
+var addJobToGrid = function (job, displayGrid, x, y, w, h) {
 
-	var box = grid.set(x, y, w, h, blessed.box, { content: job.name, style: defaultStyle } );
+	var box = displayGrid.set(x, y, w, h, blessed.box, { content: job.name, style: defaultStyle });
 	box.style.bg = job.color.split('_')[0];
 
-	if(box.style.bg === 'blue') {
+	if (box.style.bg === 'blue') {
 		box.style.bg = 'green';
 	}
 
-	if(job.color.split('_')[1] === 'anime') {
+	if (job.color.split('_')[1] === 'anime') {
 
-		setTimeout(function() {
+		setTimeout(function () {
 			box.style.bg = '#999999';
 			box.style.fg = 'black';
 			screen.render();
@@ -39,20 +39,20 @@ var addJobToGrid = function (job, grid, x, y, w, h) {
 
 var drawGrid = function (buildStatus, callback) {
 
-	var box = grid.set(0, 4, 2, 4, blessed.box, { content: moment().format('YYYY-MM-DD HH:mm:ss'), style: defaultStyle } );
+	grid.set(0, 4, 2, 4, blessed.box, { content: moment().format('YYYY-MM-DD HH:mm:ss'), style: defaultStyle });
 
 	addJobToGrid(buildStatus[0], grid, 2, 0, 5, 4);
 	addJobToGrid(buildStatus[1], grid, 2, 4, 5, 4);
 	addJobToGrid(buildStatus[2], grid, 2, 8, 5, 4);
 	addJobToGrid(buildStatus[3], grid, 7, 0, 5, 4);
 	addJobToGrid(buildStatus[4], grid, 7, 4, 5, 4);
-	addJobToGrid(buildStatus[5], grid, 7, 8, 6, 4);
+	addJobToGrid(buildStatus[5], grid, 7, 8, 5, 4);
 
 	screen.render();
 
-	setTimeout(function() {
+	setTimeout(function () {
 		return callback(drawGrid);
-	}, 2000)
+	}, 2000);
 };
 
 var filterJobs = function (jobs) {
@@ -62,7 +62,7 @@ var filterJobs = function (jobs) {
 };
 
 var getJenkinsStatus = function (callback) {
-	jenkins.info(function(err, data) {
+	jenkins.info(function (err, data) {
 		if (err) {
 			console.log(err);
 		}
@@ -71,4 +71,4 @@ var getJenkinsStatus = function (callback) {
 	});
 };
 
-getJenkinsStatus(drawGrid)
+getJenkinsStatus(drawGrid);
